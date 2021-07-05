05d418c6a95ec93329a2a4b93b99ce4ecadf526c
DrKlonk <joranminjon@gmail.com>
2021-07-05 15:22:49 +0200

    header with centered links

--- one-community/components/layout/TheHeader.vue
+++ one-community/components/layout/TheHeader.vue
@@ -1,12 +1,17 @@
 <template>
-  <header>
-    <nav>
-      <NuxtLink to="/">Community</NuxtLink>
-      <NuxtLink to="/content">Content</NuxtLink>
-      <NuxtLink to="/events">Events</NuxtLink>
-      <NuxtLink to="/contact">Contact</NuxtLink>
-      <NuxtLink to="/about">About Ordina</NuxtLink>
+  <header class="header">
+    <div class="box logo">
+      <img src="@/assets/Logo.svg" alt="Logo" />
+    </div>
+    <nav class="box nav-links">
+      <NuxtLink class="nav-link" to="/">Community</NuxtLink>
+      <NuxtLink class="nav-link" to="/content">Content</NuxtLink>
+      <NuxtLink class="nav-link" to="/events">Events</NuxtLink>
+      <NuxtLink class="nav-link" to="/contact">Get in touch</NuxtLink>
     </nav>
+    <div class="box login-box">
+      <span class="login">Login</span>
+    </div>
   </header>
 </template>
 
@@ -20,4 +25,41 @@ export default defineComponent({
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  padding: 0.5rem;
+  background-color: $gray-light;
+}
+
+.box {
+  &:last-child {
+    flex: 0 0 14rem;
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+
+.logo {
+  flex: 0 0 14rem;
+  margin-right: auto;
+}
+
+.nav-links {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+}
+
+.login {
+}
+
+.nav-link {
+  padding: 0.5rem;
+}
+
+.nuxt-link-active {
+  color: red;
+}
+</style>
