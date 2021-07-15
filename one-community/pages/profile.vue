806255816c28f208dcc6986dedaf5173bc43fc87
DrKlonk <joranminjon@gmail.com>
2021-07-15 08:57:04 +0200

    remove title on buttons

--- one-community/pages/profile.vue
+++ one-community/pages/profile.vue
@@ -2,13 +2,7 @@
   <BaseContainer>
     <h1>Logged in as {{ $auth.user.username }}</h1>
 
-    <button
-      :title="$auth.user.username"
-      class="login-box__logout"
-      @click="logout"
-    >
-      Logout
-    </button>
+    <button class="login-box__logout" @click="logout">Logout</button>
   </BaseContainer>
 </template>
 
