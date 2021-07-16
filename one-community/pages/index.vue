7c0c5d5d335afff119006b43f9ea56a537b7dbf4
DrKlonk <joranminjon@gmail.com>
2021-07-16 13:20:09 +0200

    mission placeholder

--- one-community/pages/index.vue
+++ one-community/pages/index.vue
@@ -17,17 +17,39 @@
         </ul>
       </section>
     </BaseContainer>
+    <BaseContainer class="mission narrow-grid">
+      <h2>Our mission</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi
+        incidunt voluptas atque sequi similique illo dolor exercitationem in
+        repellendus inventore consequuntur error laudantium ratione consequatur,
+        eligendi dolore odio sapiente et libero placeat, qui doloribus aliquid
+        dignissimos cupiditate. Debitis ipsam quisquam illum nisi, sit sunt
+        molestiae quidem dolores repellendus cupiditate non illo animi dolorem
+        omnis quaerat rem facilis fuga praesentium, mollitia necessitatibus
+        corporis. Deserunt perspiciatis animi magnam quibusdam aut porro at
+        quidem aliquid, minus totam dignissimos laudantium, officia, nobis sit
+        itaque dolores sequi molestiae earum magni! Quasi animi, dolorem
+        doloribus commodi, minus, sapiente minima ratione tempora veritatis quia
+        itaque nemo tenetur.
+      </p>
+      <div class="mission__buttons">
+        <BaseButton buttonType="primary">Contact</BaseButton>
+        <BaseButton buttonType="primary">Join</BaseButton>
+      </div>
+    </BaseContainer>
   </BasePageLayout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 import BaseButton from '~/components/base/BaseButton.vue'
+import BaseCard from '../components/base/BaseCard.vue'
 import BaseContainer from '../components/base/BaseContainer.vue'
 import BasePageLayout from '../components/base/BasePageLayout.vue'
 
 export default defineComponent({
-  components: { BaseButton, BaseContainer, BasePageLayout },
+  components: { BaseButton, BaseContainer, BasePageLayout, BaseCard },
   setup() {
     const coreValues = [
       {
@@ -71,5 +93,9 @@ export default defineComponent({
 
   &__item {
   }
+}
+.mission {
+  display: flex;
+  flex-direction: column;
 }
 </style>
