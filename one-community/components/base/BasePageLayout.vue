5e1472065426fd64770f44e57d02e3b348a5f515
DrKlonk <joranminjon@gmail.com>
2021-07-16 11:41:10 +0200

    enable multiple baseContainers on a page

---
+++ one-community/components/base/BasePageLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="main">
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+}
+</style>
