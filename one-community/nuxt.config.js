b97acdad4739b473493dbfb873864d87842adb10
DrKlonk <joranminjon@gmail.com>
2021-07-20 12:30:21 +0200

    remove github strategy

--- one-community/nuxt.config.js
+++ one-community/nuxt.config.js
@@ -68,11 +68,6 @@ export default {
           logout: false,
         },
       },
-      github: {
-        redirectUri: `${process.env.STRAPI_URL}/connect/github/callback`,
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      },
     },
   },
   strapi: {
