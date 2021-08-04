1e0cce0f8f7f16f55662a8e460c93aa7a21bed71
jolissa <jolissa.latour@ordina.nl>
2021-08-04 15:28:31 +0200

    start of profile page and reset password

---
+++ one-community/pages/password/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <BaseContainer>
+    <BaseForm v-if="isLoggedIn" @submit="resetPassword">
+      <template v-slot:form>
+        <h3>Reset Password</h3>
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter your email"
+          v-model="resetEmail"
+        />
+        <BaseButton buttonType="primary">Reset Password</BaseButton>
+      </template>
+    </BaseForm>
+    <BaseForm v-else @submit="forgotPassword">
+      <template v-slot:form>
+        <h3>Forgot Password</h3>
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter your email"
+          v-model="forgotEmail"
+        />
+        <BaseButton buttonType="primary">Send New Password</BaseButton>
+      </template>
+    </BaseForm>
+  </BaseContainer>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  useContext,
+  ref,
+} from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  // middleware: 'auth',
+  setup() {
+    const context = useContext()
+    const resetEmail = ref('')
+    const forgotEmail = ref('')
+
+    const isLoggedIn = computed(() => {
+      return context.$auth.$state.loggedIn
+    })
+
+    function forgotPassword() {
+      // Request API.
+      context.$axios
+        .post('http://localhost:1337/auth/forgot-password', {
+          email: forgotEmail.value, // user's email
+        })
+        .then((response) => {
+          console.log('Your user received an email')
+        })
+        .catch((error) => {
+          console.log('An error occurred:', error.response)
+        })
+    }
+
+    function resetPassword() {
+      // Request API.
+      context.$axios
+        .post('http://localhost:1337/auth/reset-password', {
+          code: 'privateCode', // code contained in the reset link of step 3.
+          password: 'userNewPassword',
+          passwordConfirmation: 'userNewPassword',
+        })
+        .then((response) => {
+          console.log("Your user's password has been reset.")
+        })
+        .catch((error) => {
+          console.log('An error occurred:', error.response)
+        })
+    }
+    return {
+      resetEmail,
+      forgotEmail,
+      isLoggedIn,
+      forgotPassword,
+      resetPassword,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
--- one-community/pages/profile.vue
+++ one-community/pages/profile.vue
@@ -1,26 +1,71 @@
 <template>
   <BasePageLayout>
-    <BaseContainer>
-      <h1>Logged in as {{ $auth.user.username }}</h1>
-
-      <button
-        :title="$auth.user.username"
-        class="login-box__logout"
-        @click="logout"
+    <BaseContainer class="profile__section--nav">
+      <p
+        class="profile__section-link"
+        :class="{ active: currentSection === 'profile' }"
+        @click="setCurrentSection('profile')"
       >
-        Logout
-      </button>
+        My Profile
+      </p>
+      <p
+        class="profile__section-link"
+        :class="{ active: currentSection === 'content' }"
+        @click="setCurrentSection('content')"
+      >
+        My Content
+      </p>
+      <p
+        class="profile__section-link"
+        :class="{ active: currentSection === 'projects' }"
+        @click="setCurrentSection('projects')"
+      >
+        My Projects
+      </p>
+    </BaseContainer>
+    <BaseContainer
+      containerType="narrow"
+      class="profile__section"
+      v-if="currentSection === 'profile'"
+    >
+      <h2>Welcome {{ $auth.user.username }}</h2>
+      <p>
+        This is your profile page where you can logout and view your profile
+        info
+      </p>
+
+      <BaseButton buttonType="primary" @click.native="logout"
+        >Logout</BaseButton
+      >
+    </BaseContainer>
+    <BaseContainer
+      containerType="narrow"
+      class="profile__section"
+      v-else-if="currentSection === 'content'"
+    >
+      <p>
+        This is an overview of the content you have produced so far
+      </p></BaseContainer
+    >
+
+    <BaseContainer
+      containerType="narrow"
+      class="profile__section"
+      v-else-if="currentSection === 'projects'"
+    >
+      <p>This is an overview of the projects you have participated in so far</p>
     </BaseContainer>
   </BasePageLayout>
 </template>
 
 <script lang="ts">
-import { defineComponent, useContext } from '@nuxtjs/composition-api'
+import { defineComponent, useContext, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   middleware: 'auth',
   setup() {
     const { $auth } = useContext()
+    const currentSection = ref('profile')
 
     // Logout
     async function logout() {
@@ -29,9 +74,39 @@ export default defineComponent({
       } catch {}
     }
 
-    return { logout }
+    function setCurrentSection(section: string) {
+      currentSection.value = section
+    }
+
+    return { logout, currentSection, setCurrentSection }
   },
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profile__section--nav.container {
+  padding-bottom: 0;
+  border-bottom: 2px solid $discovery-blue-5;
+}
+.profile__section {
+  flex-direction: column;
+  button {
+    width: fit-content;
+  }
+  p {
+    margin: 1rem 0;
+  }
+}
+.profile__section-link {
+  cursor: pointer;
+  padding: 1rem;
+  font-size: $default-font-size * 1.2;
+  border-radius: 3px 3px 0 0;
+  &:hover {
+    color: $discovery-blue-primary;
+  }
+  &.active {
+    background-color: $discovery-blue-5;
+  }
+}
+</style>
