52d301bcd94991220abd51adce96b03de974c088
DrKlonk <joranminjon@gmail.com>
2021-08-04 08:12:17 +0200

    add outlined material icons

--- one-community/nuxt.config.js
+++ one-community/nuxt.config.js
@@ -10,7 +10,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        href: 'https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp',
+        rel: 'stylesheet',
+      },
+    ],
   },
 
   publicRuntimeConfig: {
