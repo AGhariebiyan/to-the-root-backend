269545e9f848ddd58e74e252002310aa0589d378
Wim van der Veen <wim.van.der.veen@ordina.nl>
2021-08-19 14:22:36 +0200

    updated error message

--- frontend/utils/helpers.ts
+++ frontend/utils/helpers.ts
@@ -1,7 +1,7 @@
 function errorMessageFromResponse(errorResponse: any) {
   if (Array.isArray(errorResponse.response.data.message))
     return errorResponse.response.data.message[0].messages[0].message // WV 20210812 this is not fool-proof but probably good enough
-  return errorResponse.response.data.message || 'undefined error message'
+  return errorResponse.response.data.message || 'something went wrong'
 }
 
 export { errorMessageFromResponse }
