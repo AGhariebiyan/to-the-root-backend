ef84c4b7ede10105ba53e875f8ae906574b1d7e9
Jeroen Drenth <jeroen.drenth@ordina.nl>
2021-09-09 09:41:38 +0200

    Added unit tests for password reset

--- frontend/utils/helpers.ts
+++ frontend/utils/helpers.ts
@@ -1,8 +1,11 @@
-function errorMessageFromResponse(errorResponse: any) {
-  if (Array.isArray(errorResponse.response.data.message))
-    return errorResponse.response.data.message[0].messages[0].message // WV 20210812 this is not fool-proof but probably good enough
+import { AxiosError } from 'axios'
+import { ErrorResponse } from './types'
+
+function errorMessageFromResponse(errorResponse: AxiosError<ErrorResponse>) {
+  if (Array.isArray(errorResponse.response))
+    return errorResponse?.response.data.message[0].messages[0].message // WV 20210812 this is not fool-proof but probably good enough
   return (
-    errorResponse.response.data.message ||
+    errorResponse.response?.data.message ||
     'Something went wrong. Please contact a community administrator'
   )
 }
