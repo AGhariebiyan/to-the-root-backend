7da420165fe105b3fbd665a9714b04639b47e6ac
DrKlonk <joranminjon@gmail.com>
2021-09-15 13:19:18 +0200

    cleanup content/index

--- frontend/pages/content/index.vue
+++ frontend/pages/content/index.vue
@@ -1,6 +1,6 @@
 <template>
   <BasePageLayout>
-    <BaseContainer :flex-col="true">
+    <BaseContainer>
       <ais-instant-search
         :search-client="searchClient"
         :index-name="algoliaIndex"
@@ -35,14 +35,7 @@
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  ref,
-  onMounted,
-  useContext,
-  computed,
-  onUnmounted,
-} from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 
 import {
   AisConfigure,
@@ -51,10 +44,7 @@ import {
   AisSearchBox,
 } from 'vue-instantsearch'
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
-import * as _ from 'lodash'
-import { Article } from '~/utils/types'
 import algoliasearch from 'algoliasearch/lite'
-import BaseButton from '../../components/base/BaseButton.vue'
 
 export default defineComponent({
   name: 'PageContent',
@@ -65,74 +55,13 @@ export default defineComponent({
     AisInfiniteHits,
     AisSearchBox,
     ClipLoader,
-    BaseButton,
   },
 
   setup() {
-    const { store } = useContext()
-
     const limit = 6
-    const offset = ref(0)
     const query = ref('')
     const isLoading = ref(false)
 
-    const articles = computed(() => {
-      return store.getters['articles/articles']
-    })
-
-    const filteredArticles = computed(() => {
-      return articles.value.filter((article: Article) => {
-        const lowerCaseTitle = article.title.toLowerCase()
-        return lowerCaseTitle.includes(query.value.toLowerCase())
-      })
-    })
-
-    const debouncedInfiniteScroll = _.debounce(infiniteScroll, 500, {
-      leading: true,
-    })
-
-    function infiniteScroll() {
-      if (store.getters['articles/foundAllArticles']) {
-        return
-      }
-
-      const articleContainer = document.querySelector('.content__container')
-      const bottom = articleContainer?.getBoundingClientRect().bottom
-      const innerHeight = Math.round(window.innerHeight)
-
-      if (bottom && innerHeight >= bottom) {
-        offset.value += filteredArticles.value.length
-        loadArticles()
-      }
-    }
-
-    onMounted(async () => {
-      if (articles.value.length <= 1) {
-        loadArticles()
-      }
-      window.addEventListener('scroll', debouncedInfiniteScroll)
-    })
-
-    onUnmounted(() => {
-      window.removeEventListener('scroll', debouncedInfiniteScroll)
-    })
-
-    async function loadArticles() {
-      if (isLoading.value || store.getters['articles/foundAllArticles']) {
-        return
-      }
-      isLoading.value = true
-      try {
-        await store.dispatch('articles/fetchArticles', {
-          limit,
-          offset,
-        })
-      } catch (e) {
-        console.log(e)
-      } finally {
-        isLoading.value = false
-      }
-    }
     const algoliaIndex: string = process.env.algoliaIndex || ''
     const appId: string = process.env.algoliaAppId || ''
     const searchKey: string = process.env.algoliaSearchKey || ''
@@ -141,10 +70,8 @@ export default defineComponent({
 
     return {
       algoliaIndex,
-      filteredArticles,
       isLoading,
       limit,
-      loadArticles,
       query,
       searchClient,
     }
@@ -164,6 +91,7 @@ export default defineComponent({
     display: flex;
     justify-content: center;
   }
+
   &__button {
   }
 }
