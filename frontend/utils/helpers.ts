343cff0b86d4b7ff9528c041c4dfdaa9d05a4587
Jeroen Drenth <jeroen.drenth@ordina.nl>
2021-09-16 09:16:49 +0200

    Fix error handler. Still not pretty...

--- frontend/utils/helpers.ts
+++ frontend/utils/helpers.ts
@@ -1,13 +1,12 @@
 import { AxiosError } from 'axios'
-import { ErrorResponse } from './types'
 
-function errorMessageFromResponse(errorResponse: AxiosError<ErrorResponse>) {
-  if (Array.isArray(errorResponse.response))
-    return errorResponse?.response.data.message[0].messages[0].message // WV 20210812 this is not fool-proof but probably good enough
-  return (
-    errorResponse.response?.data.message ||
-    'Something went wrong. Please contact a community administrator'
-  )
+function errorMessageFromResponse(errorResponse: AxiosError<any>) {
+  const messageArray = errorResponse?.response?.data.message
+
+  return Array.isArray(messageArray)
+    ? messageArray[0].messages[0].message
+    : errorResponse.response?.data.message ||
+        'Something went wrong. Please contact a community administrator'
 }
 
 export { errorMessageFromResponse }
