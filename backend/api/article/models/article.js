240967c5b6cd6036a125eb80ce0925b0f736d803
Dennis van Oosten <dennis.van.oosten@ordina.nl>
2022-01-18 09:12:23 +0100

    updating an article kinda works

--- backend/api/article/models/article.js
+++ backend/api/article/models/article.js
@@ -1,8 +1,38 @@
 'use strict'
 
+require('dotenv').config()
+const algoliasearch = require('algoliasearch')
+const axios = require('axios')
+
+const appId = process.env.ALGOLIA_APP_ID
+const adminKey = process.env.ALGOLIA_ADMIN_KEY
+const searchClient = algoliasearch(appId, adminKey)
+const index = searchClient.initIndex(process.env.ALGOLIA_INDEX)
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {}
+module.exports = {
+    lifecycles: {
+        afterCreate: async (entry) => {
+            console.log('afterCreate');
+        },
+        afterUpdate: async (entry) => {
+            console.log('afterUpdate');
+            setArticleInAlgolia(entry);
+        },
+        afterDestroy: async (entry) => {
+            console.log('afterDestroy');
+        },
+    }
+}
+
+async function setArticleInAlgolia(entry) {
+    const article = { ...entry, objectID: entry.id }
+    delete article['comments'];
+    delete article['likes'];
+
+    index.saveObjects([article]).catch((error) => console.log(error));
+}
