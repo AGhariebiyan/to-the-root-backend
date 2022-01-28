9bbd82c2033d3403dd92a74e8d1dcd5135021d44
Dennis van Oosten <dennis.van.oosten@ordina.nl>
2022-01-28 11:47:28 +0100

    contribute page added

--- frontend/pages/contribute.vue
+++ frontend/pages/contribute.vue
@@ -3,17 +3,78 @@
     <div class="contribute-wrapper">
       <div class="wrapper">
         <h1>Contribute</h1>
+
+        <p>We're working on this...</p>
+
+        <form ref="form" class="mailing-form" @submit.prevent="sendEmail">
+          <ul class="mailing-form__error-list">
+            <li
+              class="mailing-form__error"
+              v-for="error in errors"
+              :key="error"
+            >
+              {{ error }}
+            </li>
+          </ul>
+          <div>
+            <textarea class="mailing-form__email" v-model="email" />
+          </div>
+          <div>
+            <button type="submit">Keep me updated</button>
+          </div>
+        </form>
       </div>
     </div>
   </BasePageLayout>
 </template>
 
 <script lang="ts">
-import { defineComponent, useStore } from '@nuxtjs/composition-api'
+import { defineComponent, ref, Ref } from '@nuxtjs/composition-api'
+import emailjs from '@emailjs/browser'
+// const emailServiceId = process.env.EMAILJS_SERVICE_ID
+// const emailTemplateId = process.env.EMAILJS_TEMPLATE_ID
+// const emailUserId = process.env.EMAILJS_USER_ID
 
 export default defineComponent({
   setup() {
-    const store = useStore()
+    const email = ref('')
+    const errors: Ref<string[]> = ref([])
+
+    async function sendEmail() {
+      errors.value = []
+
+      if (email.value.trim() === '') {
+        errors.value.push('Enter a valid email address!')
+        return
+      }
+
+      const templateParams = {
+        interest: 'Contribute',
+        email: email.value,
+      }
+
+      emailjs
+        .send(
+          'service_hc5nyhf',
+          'template_mailing_list',
+          templateParams,
+          'user_lvX2tiz33iKi07vdvM7a4',
+        )
+        .then(
+          (result) => {
+            console.log('SUCCESS!', result.text)
+          },
+          (error) => {
+            console.log('FAILED...', error.text)
+          },
+        )
+    }
+
+    return {
+      email,
+      sendEmail,
+      errors,
+    }
   },
 })
 </script>
@@ -39,5 +100,12 @@ export default defineComponent({
   justify-content: center;
 
   text-align: center;
+}
+
+.mailing-form {
+  &__error {
+    margin: 1rem 0;
+    color: red;
+  }
 }
 </style>
