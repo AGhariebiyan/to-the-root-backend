09a43cd41e7142c172002bfa20818a570761bdaa
Dennis van Oosten <dennis.van.oosten@ordina.nl>
2022-03-04 13:39:36 +0100

    Add name change to all files

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ToTheRoot',
+    title: 'To The Root',
     htmlAttrs: {
       lang: 'en',
     },
