77d9919bd5095b521f5774cc8eb2641bb8537439
DrKlonk <joranminjon@gmail.com>
2022-03-10 11:24:54 +0100

    Add BaseLink component

---
+++ frontend/components/base/BaseLink.vue
@@ -0,0 +1,35 @@
+<template>
+  <NuxtLink :class="type" :to="to">
+    <slot />
+  </NuxtLink>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: false,
+      default: '',
+      validator(value: string) {
+        return ['test', ''].includes(value)
+      },
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+// Add button like styling here
+.nuxt-link-active {
+  color: red;
+}
+.nuxt-link-exact-active {
+  color: blue;
+}
+</style>
