cb912d514afeee1bda87e35b4bdab1866ce9d839
DrKlonk <joranminjon@gmail.com>
2022-03-16 13:56:31 +0100

    Refactor the resizing

--- frontend/components/layout/TheHeader.vue
+++ frontend/components/layout/TheHeader.vue
@@ -1,41 +1,45 @@
 <template>
   <header class="header">
-    <div class="header__content">
-      <div class="logo">
-        <NuxtLink class="logo__link" to="/">
-          <img class="logo__image" src="@/assets/logo.svg" alt="home" />
-        </NuxtLink>
+    <template v-if="isMounted">
+      <div v-if="isLargeScreen" class="header__container--desktop">
+        <nav role="navigation" aria-label="Main" class="header__menu--desktop">
+          <TheLogo />
+
+          <NuxtLink
+            v-for="link in links"
+            :key="link.name"
+            :to="link.to"
+            class="header__menu-item"
+            >{{ link.name }}</NuxtLink
+          >
+
+          <LoginBox role="navigation" aria-label="Log in" />
+        </nav>
+        <HeaderSlashes class="header__slashes" />
       </div>
-      <span
-        role="navigation"
-        aria-label="open mobile menu"
-        v-if="!isHeaderMobileMenuActive"
-        class="material-icons header__mobile-menu-button"
-        @click="openMobileNavMenu"
-        @touch="openMobileNavMenu"
-        >menu</span
-      >
-      <span
-        role="navigation"
-        aria-label="close mobile menu"
-        v-else
-        class="material-icons header__mobile-menu-button"
-        @click="closeMobileNavMenu"
-        @touch="closeMobileNavMenu"
-        >close</span
-      >
-      <nav role="navigation" aria-label="Main" class="nav-links--desktop">
-        <NuxtLink
-          class="nav-links__item"
-          v-for="link in links"
-          :key="link.name"
-          :to="link.to"
-          >{{ link.name }}</NuxtLink
+
+      <!-- Mobile menu starts here -->
+      <div v-else class="header__container--mobile">
+        <span
+          role="navigation"
+          aria-label="open mobile menu"
+          v-if="!isMobileMenuActive"
+          class="material-icons header__mobile-menu-button"
+          @click="openMobileNavMenu"
+          @touch="openMobileNavMenu"
+          >menu</span
         >
-      </nav>
-      <LoginBox role="navigation" aria-label="Login" />
-      <div class="header__menu--mobile" v-if="isHeaderMobileMenuActive">
-        <nav role="navigation" aria-label="Main" class="nav-links--mobile">
+        <span
+          role="navigation"
+          aria-label="close mobile menu"
+          v-else
+          class="material-icons header__mobile-menu-button"
+          @click="closeMobileNavMenu"
+          @touch="closeMobileNavMenu"
+          >close</span
+        >
+
+        <nav role="navigation" aria-label="Main" class="header__menu--mobile">
           <NuxtLink
             class="nav-links__item"
             v-for="link in links"
@@ -45,10 +49,11 @@
             >{{ link.name }}</NuxtLink
           >
         </nav>
+
         <LoginBox @closeMobileMenu="closeMobileNavMenu" :isMobile="true" />
       </div>
-    </div>
-    <HeaderSlashes class="header__slashes" />
+    </template>
+    <div v-else>Loading header</div>
   </header>
 </template>
 
@@ -61,28 +66,24 @@ import {
 } from '@nuxtjs/composition-api'
 import LoginBox from '../global/LoginBox.vue'
 import HeaderSlashes from './HeaderSlashes.vue'
+import TheLogo from './TheLogo.vue'
 
 export default defineComponent({
-  components: { LoginBox, HeaderSlashes },
+  components: { LoginBox, HeaderSlashes, TheLogo },
 
   setup() {
-    function resizeHandler() {
-      let resizeInterval
-      if (isHeaderMobileMenuActive.value) {
-        resizeInterval = setInterval(closeMobileMenuOnResize, 300)
-      } else if (resizeInterval) {
-        clearInterval(resizeInterval)
-      }
-    }
+    const isMounted = ref(false)
+    const largeScreenSize = 872
+    const isLargeScreen = ref(true)
 
-    function closeMobileMenuOnResize() {
-      if (window.innerWidth >= 872) {
-        isHeaderMobileMenuActive.value = false
-      }
+    function resizeHandler() {
+      isLargeScreen.value = window.innerWidth >= largeScreenSize
     }
 
     onMounted(() => {
+      isMounted.value = true
       window.addEventListener('resize', resizeHandler)
+      isLargeScreen.value = window.innerWidth >= 872
     })
 
     onUnmounted(() => {
@@ -104,19 +105,21 @@ export default defineComponent({
       },
     ]
 
-    const isHeaderMobileMenuActive = ref(false)
+    const isMobileMenuActive = ref(false)
 
     function openMobileNavMenu() {
-      isHeaderMobileMenuActive.value = true
+      isMobileMenuActive.value = true
     }
 
     function closeMobileNavMenu() {
-      isHeaderMobileMenuActive.value = false
+      isMobileMenuActive.value = false
     }
 
     return {
       links,
-      isHeaderMobileMenuActive,
+      isLargeScreen,
+      isMobileMenuActive,
+      isMounted,
       openMobileNavMenu,
       closeMobileNavMenu,
     }
@@ -126,114 +129,26 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .header {
-  background-color: green;
+  background-color: #262626;
   height: 316px;
   position: sticky;
   top: 0;
   z-index: 50;
-}
 
-.header__content {
-  display: flex;
-  height: 261px;
-  align-items: center;
-  max-width: $desktop-max-width;
-  margin: 0 auto;
-  position: relative;
-}
+  &__container {
+    display: flex;
+    height: 261px;
+    align-items: center;
+    max-width: $desktop-max-width;
+    margin: 0 auto;
+    position: relative;
 
-.logo {
-  width: $header-item-width;
-  margin-right: auto;
-  padding: 2rem;
+    &--desktop {
+    }
 
-  &__link {
-    line-height: 0;
+    &--mobile {
+    }
   }
-
-  &__image {
-    height: 6rem;
-  }
-}
-
-.nav-links--desktop {
-  display: flex;
-  justify-content: center;
-  margin-right: 3rem;
-}
-
-.nav-links__item {
-  text-decoration: none;
-  position: relative;
-  text-transform: uppercase;
-  font-family: 'Poppins-Bold';
-  font-size: 0.9rem;
-  transition: all 0.3s ease;
-
-  &:not(:last-child) {
-    margin-right: 2.5rem;
-  }
-
-  &:link,
-  &:visited {
-    color: $gray-darkest;
-  }
-
-  &:hover {
-    color: $ordina-orange;
-  }
-
-  &.nuxt-link-exact-active::after {
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 0.35rem;
-    background-color: $gray;
-    top: $default-font-size * 2;
-    left: 0;
-  }
-}
-
-.header__menu--mobile {
-  background-color: $gray-light;
-  border-top: 1px solid $gray;
-  height: calc(100% - 4.5rem);
-  padding: 1rem;
-  position: fixed;
-  overflow-y: scroll;
-  top: 4.5rem;
-  width: 100%;
-  z-index: 50;
-}
-
-.nav-links--mobile {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 1.5rem;
-
-  .nav-links__item {
-    margin: 1.5rem 0;
-  }
-}
-
-.nav-links--mobile + .login-box {
-  width: 60%;
-  margin: 0 auto;
-  flex-direction: column;
-  padding-top: 3rem;
-  border-top: 1px solid $gray;
-}
-
-.login-box {
-  display: flex;
-  justify-content: flex-end;
-}
-
-.header__mobile-menu-button {
-  display: none;
-  cursor: pointer;
 }
 
 .header__slashes {
@@ -245,17 +160,17 @@ export default defineComponent({
 }
 
 // breaking at 872px to avoid weird underline issues
-@media only screen and (max-width: 54.5em) {
-  .nav-links--desktop {
-    display: none;
-  }
+// @media only screen and (max-width: 54.5em) {
+//   .nav-links--desktop {
+//     display: none;
+//   }
 
-  .nav-links--desktop + .login-box {
-    display: none;
-  }
+//   .nav-links--desktop + .login-box {
+//     display: none;
+//   }
 
-  .header__mobile-menu-button {
-    display: flex;
-  }
-}
+//   .header__mobile-menu-button {
+//     display: flex;
+//   }
+// }
 </style>
---
+++ frontend/components/layout/TheLogo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="logo">
+    <NuxtLink class="logo__link" to="/">
+      <img
+        v-if="isFlat"
+        class="logo__image"
+        src="@/assets/logo.svg"
+        alt="home"
+      />
+      <img v-else class="logo__image" src="@/assets/logo.svg" alt="home" />
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isFlat: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  padding: 2rem;
+
+  &__link {
+  }
+
+  &__image {
+    height: 6rem;
+  }
+}
+</style>
