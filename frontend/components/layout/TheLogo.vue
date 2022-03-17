1eebf66f81645edc9738e1b36ad00eb2f46598f4
DrKlonk <joranminjon@gmail.com>
2022-03-17 18:14:22 +0100

    Use background image on logo

--- frontend/components/layout/TheLogo.vue
+++ frontend/components/layout/TheLogo.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="logo" :class="{ 'logo--flat': isFlat }">
     <NuxtLink class="logo__link" to="/">
-      <img
-        v-if="isFlat"
-        class="logo__image"
-        src="@/assets/logo_dianegative_horizontal.svg"
-        alt="home"
-      />
-      <img
-        v-else
-        class="logo__image"
-        src="@/assets/logo_dianeg.svg"
-        alt="home"
-      />
+      <div class="logo__img" />
     </NuxtLink>
   </div>
 </template>
@@ -36,8 +25,10 @@ export default {
   &--flat {
     padding: 1rem;
 
-    & .logo__image {
-      height: 3rem;
+    & .logo__img {
+      background-image: url(~/assets/logo_dianegative_horizontal.svg);
+      height: 50px;
+      width: 152px;
     }
   }
 
@@ -46,6 +37,14 @@ export default {
 
   &__image {
     height: 7rem;
+  }
+
+  &__img {
+    background-image: url(~/assets/logo_dianeg.svg);
+    background-size: cover;
+    height: 112px;
+    width: 135px;
+    transition: all $header-transition-time ease;
   }
 }
 </style>
