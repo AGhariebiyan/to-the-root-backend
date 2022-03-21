00c0d3d20f1ab36aecc1b0f90d593fd7c0611f5b
youssef <youssef.kassem@ordina.nl>
2022-03-21 09:33:49 +0100

    adding configs to azure storage

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -10,6 +10,18 @@ module.exports = ({ env }) => ({
       defaultReplyTo: 'totherootdev@gmail.com',
       testAddress: 'yourname@ordina.nl',
     },
+    upload: {
+      provider: 'azure-storage',
+      providerOptions: {
+        account: env('STORAGE_ACCOUNT'),
+        accountKey: env('STORAGE_ACCOUNT_KEY'),
+        serviceBaseURL: env('STORAGE_URL'),
+        containerName: env('STORAGE_CONTAINER_NAME'),
+        cdnBaseURL: env('STORAGE_CDN_URL'),
+        defaultPath: 'assets',
+        maxConcurrent: 10
+      }
+    }
   },
   // ...
 });
