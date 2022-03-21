c1dc744e14a98eab00a57729ce2464b7b60cdd7a
youssef <youssef.kassem@ordina.nl>
2022-03-21 10:28:28 +0100

    new config for azure store

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -9,18 +9,6 @@ module.exports = ({ env }) => ({
       defaultFrom: 'totherootdev@gmail.com',
       defaultReplyTo: 'totherootdev@gmail.com',
       testAddress: 'yourname@ordina.nl',
-    },
-    upload: {
-      provider: 'azure-storage',
-      providerOptions: {
-        account: env('STORAGE_ACCOUNT'),
-        accountKey: env('STORAGE_ACCOUNT_KEY'),
-        serviceBaseURL: env('STORAGE_URL'),
-        containerName: env('STORAGE_CONTAINER_NAME'),
-        cdnBaseURL: env('STORAGE_CDN_URL'),
-        defaultPath: 'assets',
-        maxConcurrent: 10
-      }
     }
   },
   // ...
