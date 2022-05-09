fde6f52fe85bafd8cef03580a55446600ceccb82
DrKlonk <joranminjon@gmail.com>
2022-05-09 11:17:24 +0200

    Return query functionality to articles, events, featureds

--- api/event/controllers/event.js
+++ api/event/controllers/event.js
@@ -1,25 +1,37 @@
-const { sanitizeEntity } = require('strapi-utils');
-const { transformUserForEventPage } = require('../../../extensions/users-permissions/services/sanitize-user');
+const { sanitizeEntity } = require('strapi-utils')
+const {
+  transformUserForEventPage,
+} = require('../../../extensions/users-permissions/services/sanitize-user')
 
 module.exports = {
-    async find() {
-        const entities = await strapi.services.event.find();
+  async find(ctx) {
+    let events
 
-        return entities.map(entity => {
-            if (entity.speakers) {
-                entity.speakers = entity.speakers.map(speaker => transformUserForEventPage(speaker))
-            }
-            return sanitizeEntity(entity, { model: strapi.models.event });
-        })
-    },
+    if (ctx.query._q) {
+      events = await strapi.services.event.search(ctx.query)
+    } else {
+      events = await strapi.services.event.find(ctx.query)
+    }
 
-    async findOne(ctx) {
-        const entity = await strapi.services.event.findOne({ id: ctx.params.id });
+    return events.map((event) => {
+      if (event.speakers) {
+        event.speakers = event.speakers.map((speaker) =>
+          transformUserForEventPage(speaker),
+        )
+      }
+      return sanitizeEntity(event, { model: strapi.models.event })
+    })
+  },
 
-        if (entity.speakers) {
-            entity.speakers = entity.speakers.map(speaker => transformUserForEventPage(speaker))
-        }
+  async findOne(ctx) {
+    const entity = await strapi.services.event.findOne({ id: ctx.params.id })
 
-        return sanitizeEntity(entity, { model: strapi.models.event });
-    },
-};
+    if (entity.speakers) {
+      entity.speakers = entity.speakers.map((speaker) =>
+        transformUserForEventPage(speaker),
+      )
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.event })
+  },
+}
--- api/featured/controllers/featured.js
+++ api/featured/controllers/featured.js
@@ -1,7 +1,9 @@
-'use strict';
+'use strict'
 
-const { sanitizeEntity } = require("strapi-utils/lib");
-const { transformUserForArticlePage } = require("../../../extensions/users-permissions/services/sanitize-user");
+const { sanitizeEntity } = require('strapi-utils/lib')
+const {
+  transformUserForArticlePage,
+} = require('../../../extensions/users-permissions/services/sanitize-user')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -9,14 +11,22 @@ const { transformUserForArticlePage } = require("../../../extensions/users-permi
  */
 
 module.exports = {
-    async find() {
-        const featureds = await strapi.services.featured.find();
+  async find(ctx) {
+    let featureds
 
-        return featureds.map(featured => {
-            if (featured.article.author) {
-                featured.article.author = transformUserForArticlePage(featured.article.author)
-            }
-            return sanitizeEntity(featured, { model: strapi.models.article });
-        })
-    },
-};
+    if (ctx.query._q) {
+      featureds = await strapi.services.featured.search(ctx.query)
+    } else {
+      featureds = await strapi.services.featured.find(ctx.query)
+    }
+
+    return featureds.map((featured) => {
+      if (featured.article.author) {
+        featured.article.author = transformUserForArticlePage(
+          featured.article.author,
+        )
+      }
+      return sanitizeEntity(featured, { model: strapi.models.article })
+    })
+  },
+}
